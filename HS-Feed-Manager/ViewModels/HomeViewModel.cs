94e03ef0cd9b18bd291526c7dd606fab202553e4
JustForFunDeveloper <tappler.andreas@gmail.com>
2019-04-03 21:31:35 +0200

    * Finished design of the HomeView

--- HS-Feed-Manager/ViewModels/HomeViewModel.cs
+++ HS-Feed-Manager/ViewModels/HomeViewModel.cs
@@ -1,12 +1,33 @@
-﻿namespace HS_Feed_Manager.ViewModels
+﻿using MahApps.Metro.IconPacks;
+using System.Collections.Generic;
+
+namespace HS_Feed_Manager.ViewModels
 {
     public class HomeViewModel : PropertyChangedViewModel
     {
         private readonly PropertyChangedViewModel _mainViewModel;
 
+        private List<string> _icons = new List<string> {
+            PackIconMaterialDesignKind.StarBorder.ToString(),
+            PackIconMaterialDesignKind.StarBorder.ToString(),
+            PackIconMaterialDesignKind.StarBorder.ToString(),
+            PackIconMaterialDesignKind.StarBorder.ToString(),
+            PackIconMaterialDesignKind.StarBorder.ToString() };
+
         public HomeViewModel(PropertyChangedViewModel mainViewModel)
         {
             _mainViewModel = mainViewModel;
+            Icons[0] = PackIconMaterialDesignKind.Star.ToString();
+        }
+
+        public List<string> Icons
+        {
+            get => _icons;
+            set
+            {
+                _icons = value;
+                OnPropertyChanged();
+            }
         }
     }
 }
